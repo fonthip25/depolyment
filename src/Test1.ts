79f260a8b1015f1bbd1016e71fc08d97a53f2beb
fonthip25 <151113299+fonthip25@users.noreply.github.com>
2024-10-01 15:10:03 +0700

    done

--- src/Test1.ts
+++ src/Test1.ts
@@ -1,8 +1,52 @@
 import { Utils } from "./Utils";
+import axios from "axios";
 
+const unit_test = async ()=>{
+
+//test1
 if(Utils.add(1,2) === 3)
 {
     console.log(0)
 }else{
-    console.log(1)
+    console.log('add test1')
+    return;
 }
+
+//test2
+if(Utils.add(2,2) === 4)
+    {
+        console.log(0)
+    }else{
+        console.log('add test2')
+        return;
+    }
+
+    const data:any = {
+        "name" : "Noey",
+        "username" : "iamnoey"
+    }
+
+//try
+    try {
+
+    const response:any = await Utils.addUser(data);
+    const correct_result: any = {"name" : "Noey","username" : "iamnoey"}
+
+    const keys:string[] = Object.keys(correct_result);
+    for(const k of keys )
+    {
+        if(correct_result[k] !== response.data[k])
+        {
+            console.log("addUser" + k)
+            return
+        }
+    }
+
+    }
+    catch (error) {
+        console.log("addUser" + error)
+    }
+
+}
+
+unit_test();
